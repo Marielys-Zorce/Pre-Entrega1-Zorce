66fadb2e9407fc047ffcf4060ac67848857a57f7
Marielys Zorce <mzorce@gmail.com>
2022-12-21 14:15:04 -0300

    Pre-entrega1-Zorce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar/NavBar';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+
+
+
 
 function App() {
+  const torta = {
+    Descripcion: "Torta de Chocolate",
+    Precio: "$ 16.000",
+    Img: "../IMG3.jpg",
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <ItemListContainer Descripcion={torta.Descripcion} Precio={torta.Precio} Img={torta.Img} />
+
     </div>
   );
 }
